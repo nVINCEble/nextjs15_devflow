f528bd5ab86845848e5c98619002212ee8068630
Vince <nVINCEble@users.noreply.github.com>
2024-12-05 10:42:05 -0800

    Implement Question Card

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,5 @@
 import { auth } from "@/auth";
+import QuestionCard from "@/components/cards/QuestionCard";
 import HomeFilter from "@/components/filters/HomeFilter";
 import LocalSearch from "@/components/search/LocalSearch";
 import { Button } from "@/components/ui/button";
@@ -14,7 +15,12 @@ const questions = [
       { _id: "1", name: "React" },
       { _id: "2", name: "JavaScript" },
     ],
-    author: { _id: "1", name: "John Doe" },
+    author: {
+      _id: "2",
+      name: "Jane Doe",
+      image:
+        "https://static.vecteezy.com/system/resources/thumbnails/002/002/257/small/beautiful-woman-avatar-character-icon-free-vector.jpg",
+    },
     upvotes: 10,
     answers: 5,
     views: 100,
@@ -28,11 +34,16 @@ const questions = [
       { _id: "1", name: "JavaScript" },
       { _id: "2", name: "JavaScript" },
     ],
-    author: { _id: "1", name: "John Doe" },
+    author: {
+      _id: "1",
+      name: "John Doe",
+      image:
+        "https://png.pngtree.com/png-vector/20191101/ourmid/pngtree-cartoon-color-simple-male-avatar-png-image_1934459.jpg",
+    },
     upvotes: 10,
     answers: 5,
     views: 100,
-    createdAt: new Date(),
+    createdAt: new Date("2021-09-01"),
   },
 ];
 
@@ -69,7 +80,7 @@ const Home = async ({ searchParams }: SearchParams) => {
       <HomeFilter />
       <div className="mt-10 flex w-full flex-col gap-6">
         {filteredQuestions.map((question) => (
-          <h1 key={question._id}>{question.title}</h1>
+          <QuestionCard key={question._id} question={question} />
         ))}
       </div>
     </>
--- lib/utils.ts
+++ lib/utils.ts
@@ -13,3 +13,38 @@ export const getDeviconClassName = (techname: string) => {
     ? `${techMap[normalizedTechName]} colored`
     : "devicon-devicon-plain";
 };
+
+export const getTimeStamp = (date: Date): string => {
+  const now = new Date();
+  const timeStamp = date.getTime();
+  const currentTimeStamp = now.getTime();
+
+  const differenceInSeconds = Math.floor((currentTimeStamp - timeStamp) / 1000);
+
+  if (differenceInSeconds < 60) {
+    return `${differenceInSeconds} second${differenceInSeconds !== 1 ? "s" : ""} ago`;
+  }
+
+  const differenceInMinutes = Math.floor(differenceInSeconds / 60);
+  if (differenceInMinutes < 60) {
+    return `${differenceInMinutes} minute${differenceInMinutes !== 1 ? "s" : ""} ago`;
+  }
+
+  const differenceInHours = Math.floor(differenceInMinutes / 60);
+  if (differenceInHours < 24) {
+    return `${differenceInHours} hour${differenceInHours !== 1 ? "s" : ""} ago`;
+  }
+
+  const differenceInDays = Math.floor(differenceInHours / 24);
+  if (differenceInDays < 30) {
+    return `${differenceInDays} day${differenceInDays !== 1 ? "s" : ""} ago`;
+  }
+
+  const differenceInMonths = Math.floor(differenceInDays / 30);
+  if (differenceInMonths < 12) {
+    return `${differenceInMonths} month${differenceInMonths !== 1 ? "s" : ""} ago`;
+  }
+
+  const differenceInYears = Math.floor(differenceInMonths / 12);
+  return `${differenceInYears} year${differenceInYears !== 1 ? "s" : ""} ago`;
+};
