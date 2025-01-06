44514c2633991e9f72718a22379325668dc93a7e
Vince <nVINCEble@users.noreply.github.com>
2025-01-06 12:00:39 -0800

    Implement Logging

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,7 +4,9 @@ import HomeFilter from "@/components/filters/HomeFilter";
 import LocalSearch from "@/components/search/LocalSearch";
 import { Button } from "@/components/ui/button";
 import ROUTES from "@/constants/routes";
-
+import handleError from "@/lib/handlers/error";
+import { NotFoundError, ValidationError } from "@/lib/http-errors";
+import dbConnect from "@/lib/mongoose";
 import Link from "next/link";
 
 const questions = [
@@ -48,11 +50,21 @@ const questions = [
   },
 ];
 
+const test = async () => {
+  try {
+    throw new Error("This is a test error");
+  } catch (error) {
+    return handleError(error);
+  }
+};
+
 interface SearchParams {
   searchParams: Promise<{ [key: string]: string }>;
 }
 
 const Home = async ({ searchParams }: SearchParams) => {
+  const result = await test();
+
   const { query = "", filter = "" } = await searchParams;
   const filteredQuestions = questions.filter((question) => {
     const matchesQuery = question.title.toLowerCase().includes(query.toLowerCase());
--- lib/handlers/error.ts
+++ lib/handlers/error.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
 import { RequestError, ValidationError } from "../http-errors";
 import { ZodError } from "zod";
+import logger from "../logger";
 
 export type ResponseType = "api" | "server";
 
@@ -24,6 +25,7 @@ const formatResponse = (
 
 const handleError = (error: unknown, responseType: ResponseType = "server") => {
   if (error instanceof RequestError) {
+    logger.error({ err: error }, `${responseType.toUpperCase()} ERROR: ${error.message}`);
     return formatResponse(responseType, error.statusCode, error.message, error.errors);
   }
 
@@ -31,6 +33,7 @@ const handleError = (error: unknown, responseType: ResponseType = "server") => {
     const validationError = new ValidationError(
       error.flatten().fieldErrors as Record<string, string[]>
     );
+    logger.error({ err: error }, `Validation error: ${validationError.message}`);
     return formatResponse(
       responseType,
       validationError.statusCode,
@@ -40,8 +43,10 @@ const handleError = (error: unknown, responseType: ResponseType = "server") => {
   }
 
   if (error instanceof Error) {
+    logger.error(error.message);
     return formatResponse(responseType, 500, error.message);
   }
+  logger.error({ err: error }, "An expected error occurred");
   return formatResponse(responseType, 500, "An expected error occurred");
 };
 
